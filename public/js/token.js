4347b2120312e87f64ad06327e75ae041251f130
Jake <mjakerick@gmail.com>
2023-03-30 14:29:47 -0500

    moved common functions from form.js to token.js

--- public/js/token.js
+++ public/js/token.js
@@ -22,3 +22,40 @@ const compareToken = (token, key) => {
     }
     return false;
 }
+
+// common functions
+
+// send data function
+const sendData = (path, data) => {
+    fetch(path, {
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify(data)
+    }).then((res) => res.json())
+    .then(response => {
+        processData(response);
+    })
+}
+
+const processData = (data) => {
+    loader.style.display = null;
+    if(data.alert){
+        showAlert(data.alert);
+    } else if(data.name) {
+        // create authToken
+        data.authToken = generateToken(data.email);
+        sessionStorage.user = JSON.stringify(data);
+        location.replace('/');
+    }
+}
+
+// alert function
+const showAlert = (msg) => {
+    let alertBox = document.querySelector('.alert-box');
+    let alertMsg = document.querySelector('.alert-msg');
+    alertMsg.innerHTML = msg;
+    alertBox.classList.add('show');
+    setTimeout(() => {
+        alertBox.classList.remove('show');
+    }, 3000);
+}
