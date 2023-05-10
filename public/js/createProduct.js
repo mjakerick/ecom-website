fa6c60c29e49534aa3af9b7bd84ddbf65e8842b1
Jake <mjakerick@gmail.com>
2023-05-09 20:22:26 -0500

    data is now loaded from db for product info on seller page

--- public/js/createProduct.js
+++ public/js/createProduct.js
@@ -3,16 +3,17 @@ const createProduct = (data) => {
   productContainer.innerHTML += `
   <div class="product-card">
     <div class="product-image">
-      <span class="tag">Draft</span>
-      <img src="img/card1.png" class="product-thumb" alt="">
+      ${data.draft ? `<span class="tag">Draft</span>` : ''}
+      <img src="${data.images[0]}" class="product-thumb" alt="">
       <button class="card-action-btn edit-btn"><img src="img/edit.png"></button>
       <button class="card-action-btn open-btn"><img src="img/open.png"></button>
       <button class="card-action-btn delete-popup-btn"><img src="img/delete.png"></button>
     </div>
     <div class="product-info">
-      <h2 class="product-brand">brand</h2>
-      <p class="product-short-des">a short line about the clothing..</p>
-      <span class="price">$20</span><span class="actual-price">$40</span>
+      <h2 class="product-brand">${data.name}</h2>
+      <p class="product-short-des">${data.shortDes}</p>
+      <span class="price">$${data.sellPrice}</span>
+      <span class="actual-price">$${data.actualPrice}</span>
     </div>
   </div>
   `;
